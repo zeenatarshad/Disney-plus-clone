0ba544eab2e4e23bffbcdb8786b48aa8adc8d737
zeenatarshad <zputhawala3011@gmail.com>
2022-07-02 21:09:36 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'loki',
+        des: "Loki, Prince of Asgard, Odinson, rightful heir of Jotunheim, and God of Mischief, is burdened with glorious purpose. His desire to be a king drives him to sow chaos in Asgard. In his lust for power, he extends his reach to Earth.",
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'falcon and the winter soldier',
+        des: "Following the events of Avengers: Endgame, Sam Wilson/Falcon and Bucky Barnes/Winter Soldier team up in a global adventure that tests their abilities — and their patience — in Marvel Studios' The Falcon and The Winter Soldier.",
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: "WandaVision follows Wanda Maximoff and Vision as they live an idyllic suburban life in the town of Westview, New Jersey, until their reality starts moving through different decades of sitcom homages and television tropes.",
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. However, when sinister monsters known as the Druun threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, those same monsters have returned, and it's up to a lone warrior to track down the last dragon and stop the Druun for good.",
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: "Set in a beautiful seaside town on the Italian Riviera, the original animated feature is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gelato, pasta and endless scooter rides. Luca shares these adventures with his newfound best friend, but all the fun is threatened by a deeply held secret: he is a sea monster from another world just below the water's surface.",
+        image: 'images/slider 5.PNG'
+    }
+]
